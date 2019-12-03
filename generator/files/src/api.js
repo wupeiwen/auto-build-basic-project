a2e4844d138ef2ef677500265fdb7f2e37183bd1
wupeiwen <javapeiwen2010@gmail.com>
2019-12-03 10:08:28 +0800

    fix: 修复变量LOCATION无效的问题

--- generator/files/src/api.js
+++ generator/files/src/api.js
@@ -2,13 +2,13 @@
  * @Author: wupeiwen javapeiwen2010@gmail.com
  * @Date: 2018-09-18 10:19:23
  * @Last Modified by: wupeiwen javapeiwen2010@gmail.com
- * @Last Modified time: 2019-12-03 10:04:37
+ * @Last Modified time: 2019-12-03 10:07:45
  */
 
 //  引入httpUtil
 import { get, post } from '@/plugins/httpUtil'
 // 基本路径
-const baseUrl = process.env.LOCATION
+const baseUrl = process.env.VUE_APP_LOCATION
 
 // 接口示例1
 export const api1 = (params) => get(`${baseUrl}/1`, params)
