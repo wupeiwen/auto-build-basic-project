4ea7025f76ffbc03e5db43abc7f5d4a28271fba8
wupeiwen <javapeiwen2010@gmail.com>
2020-12-16 16:41:34 +0800

    fix: 修复缺少vuex文件的问题

---
+++ generator/template/src/store/index.js
@@ -0,0 +1,18 @@
+/*
+ * @Author: wupeiwen <javapeiwen2010@gmail.com>
+ * @Date: 2020-12-16 16:38:31
+ * @LastEditors: wupeiwen <javapeiwen2010@gmail.com>
+ * @LastEditTime: 2020-12-16 16:39:09
+ */
+import Vue from 'vue'
+import Vuex from 'vuex'
+
+Vue.use(Vuex)
+
+export default new Vuex.Store({
+  state: {},
+  getters: {},
+  mutations: {},
+  actions: {},
+  modules: {}
+})
