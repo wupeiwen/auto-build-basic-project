58d6cbcc6b437ab913e79541ff095e63ace4d273
wupeiwen <javapeiwen2010@gmail.com>
2020-12-29 22:31:35 +0800

    🐛 fix: 修复组件自动注册失效的问题

--- generator/template/src/components/regist.js
+++ generator/template/src/components/regist.js
@@ -4,11 +4,11 @@ import camelCase from 'lodash/camelCase'
 
 const requireComponent = require.context(
   // 其组件目录的相对路径
-  './',
+  '@/components/',
   // 是否查询其子目录
   true,
   // 匹配基础组件文件名的正则表达式
-  /[A-Z]\w+\.(vue|jsx)$/
+  /\w+\.(vue|jsx)$/
 )
 requireComponent.keys().forEach(fileName => {
   // 获取组件配置
