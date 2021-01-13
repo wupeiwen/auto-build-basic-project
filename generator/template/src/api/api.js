7fac14b769d9ddca94fa12ec3732467789ba902c
wupeiwen <javapeiwen2010@gmail.com>
2021-01-13 09:07:31 +0800

    refactor: 调整数据接口模块封装(api.js)

--- generator/template/src/api/api.js
+++ generator/template/src/api/api.js
@@ -3,9 +3,13 @@ import { get, post } from '@/api/httpUtil'
 // 基本路径
 const baseUrl = process.env.VUE_APP_LOCATION
 
+const api = {}
+
 // 接口示例1
-export const api1 = (params) => get({url: `${baseUrl}/1`, params: params})
+api.api1 = (params) => get({url: `${baseUrl}/1`, params: params})
 // 接口示例2
-export const api2 = (params) => post({url:`${baseUrl}/2`, params: params, responseType: 'json'})
+api.api2 = (params) => post({url:`${baseUrl}/2`, params: params, responseType: 'json'})
 // 接口示例3
-export const api3 = (params) => post({url:`${baseUrl}/3`, params: params, responseType: 'json', headers: {'Content-Type': 'multipart/form-data'}})
+api.api3 = (params) => post({url:`${baseUrl}/3`, params: params, responseType: 'json', headers: {'Content-Type': 'multipart/form-data'}})
+
+export default api
